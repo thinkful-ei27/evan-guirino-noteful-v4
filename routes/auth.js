2931d365967d3496623b72761c56ae5a957171b0
Evan Guirino <etg624@gmail.com>
2019-01-15 12:09:12 -0800

    created webtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
 const options = { session: false, failWithError: true };
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) {
+  return jwt.sign({user}, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 router.post('/', localAuth, (req, res) => {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
 });
 
 module.exports = router;
